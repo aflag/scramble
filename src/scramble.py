2e964790286e04916a145871ac9c293166899e32
Rafael Cunha de Almeida <rafael.almeida@ef.com>
2017-12-09 17:22:25 +0000

    Improve readability

--- src/scramble.py
+++ src/scramble.py
@@ -29,8 +29,12 @@ def get_anagram_distance(s1, s2):
     assert len(s1) == len(s2)
     s1_digrams = _digrams(s1)
     s2_digrams = _digrams(s2)
-    intersection = s1_digrams.intersection(s2_digrams)
-    return len(s1_digrams) - len(intersection)
+    for e in s2_digrams:
+        try:
+            s1_digrams.remove(e)
+        except ValueError:
+            pass
+    return len(s1_digrams)
 
 
 def best_scramble(s):
@@ -49,12 +53,13 @@ def scramble(s):
     """This is a greedy algorithm which attempts to compute the most distant
     string from s, as measured with get_anagram_distance. It may result in
     suboptimal results. The run time is O(n^2)"""
-    digrams = _digrams(s)
+    digrams = set(_digrams(s))
     scrambled = list(s)
     for i in range(len(scrambled)):
         for j in range(i, len(scrambled)):
             if (_prev(scrambled, i), scrambled[j]) not in digrams:
                 scrambled[i], scrambled[j] = scrambled[j], scrambled[i]
+                break
     return ''.join(scrambled)
 
 
@@ -67,12 +72,12 @@ def _prev(scrambled, i):
 
 def _digrams(s):
     if not s:
-        return set()
+        return []
     reverse = s[::-1]
-    d_normal = set(zip(s, s[1:]))
-    d_reversed = set(zip(reverse, reverse[1:]))
-    d_border = {("", s[0]), (s[-1], "")}
-    return d_normal.union(d_reversed).union(d_border)
+    d_normal = list(zip(s, s[1:]))
+    d_reverse = list(zip(reverse, reverse[1:]))
+    d_border = [("", s[0]), (s[-1], "")]
+    return d_normal + d_reverse + d_border
 
 
 def _swaps(s):
